f0ded77e27628b8c9318bf4d7b18eb7cae21f974
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-10-16 23:27:57 +0300

    page titles + user titles count + etc.

---
+++ AniListVisualizer/Services/Helpers.cs
@@ -0,0 +1,6 @@
+namespace AniListVisualizer.Services;
+
+public static class Helpers
+{
+    public static string Ending_ENG(int number, string word) => $"{number} {word}{(number == 1 ? "" : "s")}";
+}
