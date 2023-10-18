828443754c72f42cd03856bbf699e7a2567b2812
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-10-18 19:35:15 +0300

    Model.Years

--- AniListVisualizer/Controllers/UserController.cs
+++ AniListVisualizer/Controllers/UserController.cs
@@ -13,8 +13,8 @@ public class UserController : Controller
         _logger = logger;
     }
     
-    [Route("user/{username}")]
-    public async Task<IActionResult> Index(string username)
+    [Route("user/{username}/{from?}/{to?}")]
+    public async Task<IActionResult> Index(string username, int? from, int? to)
     {
         try
         {
@@ -23,6 +23,16 @@ public class UserController : Controller
         
             var baka = new AniListExtractor();
             var user = await baka.GetUserViewModel(username);
+
+            if (from is not null)
+            {
+                var same = to is null;
+
+                var min = same ? from.Value : Math.Min(from.Value, to!.Value);
+                var max = same ? from.Value : Math.Max(from.Value, to!.Value);
+
+                user.Years = same ? new HashSet<int> { min } : Enumerable.Range(min, max - min + 1).ToHashSet();
+            }
 
             timer.Stop();
             _logger.LogInformation("USER: {name}. ENTRIES: {count}. TIME: {time:m\\:ss\\.fff}", user.User.name, user.History.Count, timer.Elapsed);
