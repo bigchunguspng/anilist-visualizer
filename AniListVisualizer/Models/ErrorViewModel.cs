12e7640668d0049da76277e891eae6712f785513
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-02-02 23:26:54 +0200

    remove old version

--- AniListVisualizer/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AniListVisualizer.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
